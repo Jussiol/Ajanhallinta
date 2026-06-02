5b2acba6878e0d092d9dddb6c3b6b316f9c29d38
Jussiol <201236226+Jussiol@users.noreply.github.com>
2026-06-02 16:55:28 +0300

    Lisätty tilasto

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-from tiedoston_kasittely import lue_asetukset
+from tiedoston_kasittely import lue_asetukset, tilastoon, tulosta_tilasto, tilasto_init
 from valitse_FIFO import valinta
 
 print("Enter jos valmis, 0 lopettaa")
+print("t = tilasto, r = nollaa tilasto")
+
 while True:
     asetukset = lue_asetukset()
     #Valitaan aktiviteetti
     aktiviteetti = valinta(asetukset, "aktiviteetit")
+    
+    try:
+        tilastoon(aktiviteetti)
+    except:
+        tilasto_init(asetukset)
+        tilastoon(aktiviteetti)
+
     if aktiviteetti == "projekti":
         aktiviteetti = valinta(asetukset, "projektit")
 
     print(f"{aktiviteetti}")
     
     done = input("Done? ")
+    if done == "t":
+        tulosta_tilasto()
+        done = input("Done? ")
     if done == "0":
         break
+    if done == "r":
+        tilasto_init(asetukset)
+        
     
--- tiedoston_kasittely.py
+++ tiedoston_kasittely.py
@@ -1,18 +1,48 @@
 import json
 from pathlib import Path
 
+def base_dir():
+    polku = Path(__file__).resolve().parent
+    return polku
+
 def lue_asetukset():
     #avataan asetukset
-    BASE_DIR = Path(__file__).resolve().parent
-    ASETUKSET_POLKU = BASE_DIR / "asetukset.json"
-    
-    with open(ASETUKSET_POLKU, encoding= "utf-8") as j:
+
+    with open(base_dir() / "asetukset.json", encoding= "utf-8") as j:
         asetukset = json.load(j)
     return asetukset
 
 def kirjoita_asetukset(asetukset, laji, vaihtoehdot, estetyt):
+    #avataan asetukset
+
     asetukset[laji]["vaihtoehdot"] = vaihtoehdot
     asetukset[laji]["estetyt"] = estetyt
 
-    with open("asetukset.json", "w", encoding="utf-8") as j:
+    with open(base_dir()  / "asetukset.json", "w", encoding="utf-8") as j:
         json.dump(asetukset, j, ensure_ascii=False, indent=4)
+
+def tilasto_init(asetukset):
+    tilasto = {}
+    for item in asetukset["aktiviteetit"]["kaikki"]:
+        tilasto[item] = 0
+    with open(base_dir()  / "tilasto.json", "w", encoding="utf-8") as j:
+        json.dump(tilasto, j, ensure_ascii=False, indent=4)
+
+def tilastoon(aktiviteetti):
+    #avataan asetukset
+    with open(base_dir() / "tilasto.json", encoding= "utf-8") as j:
+        tilasto = json.load(j)
+    tilasto[aktiviteetti] += 1
+    with open(base_dir()  / "tilasto.json", "w", encoding="utf-8") as j:
+        json.dump(tilasto, j, ensure_ascii=False, indent=4)
+
+def tulosta_tilasto():
+        
+    #avataan asetukset
+    with open(base_dir() / "tilasto.json", encoding= "utf-8") as j:
+        tilasto = json.load(j)
+    print()
+    print("Tilasto:")
+    for avain, arvo in tilasto.items():
+        print("   -", avain, arvo)
+    print()
